ce1ee4ba1591bc0c236166b690728069032b1c0e
Tom <tom@lant.uk>
2019-10-19 01:17:02 +0100

    fix typoo

--- build_lambda.sh
+++ build_lambda.sh
@@ -2,7 +2,7 @@
 rm -rf ./build_func
 pip install -r requirements.txt --target ./build_func
 cp riots.py ./build_func/
-cp -r sites ./build_func/sites/
+cp -r site ./build_func/site/
 cd build_func
 zip -r9 ../lambda.zip .
 cd ..
